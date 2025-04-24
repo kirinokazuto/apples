af5056bf4084699d03227da7a80b171eadded2f2
DESKTOP-CMFB67U\GA2A <h1202419004@hiratagakuen.onmicrosoft.com>
2025-04-24 16:02:18 +0900

    タイトル画面からゲームシーンに移動

---
+++ Flip&Draw/Assets/kirino/Titleidou.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Titleidou : MonoBehaviour
+{
+    public void change_button() //change_button‚Æ‚¢‚¤–¼‘O‚É‚µ‚Ü‚·
+    {
+        SceneManager.LoadScene("Game");//second‚ğŒÄ‚Ño‚µ‚Ü‚·
+    }
+}
