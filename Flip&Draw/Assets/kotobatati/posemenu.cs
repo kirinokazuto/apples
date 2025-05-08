1bbdecd2030b161968a5a155e5f61fabe13771c4
DESKTOP-CMFB67U\GA2A <h1202419004@hiratagakuen.onmicrosoft.com>
2025-05-08 15:42:34 +0900

    エラーありpose設定

---
+++ Flip&Draw/Assets/kotobatati/posemenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class posemenu : MonoBehaviour
+{
+    public void change_button() //change_button‚Æ‚¢‚¤–¼‘O‚É‚µ‚Ü‚·
+    {
+        SceneManager.LoadScene("Pose");//second‚ğŒÄ‚Ño‚µ‚Ü‚·
+    }
+}
